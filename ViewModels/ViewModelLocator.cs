ab4111a5a95e5b6f4085d2d344b154941936a3d8
weimin.zhang <409347105@qq.com>
2024-02-22 21:12:11 +0800

    Add ViewModelLocator

---
+++ ViewModels/ViewModelLocator.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using CommunityToolkit.Mvvm.Messaging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DictionaryEditor.ViewModels
+{
+    /// <summary>
+    /// This class contains static references to all the view models in the
+    /// application and provides an entry point for the bindings.
+    /// </summary>
+    public class ViewModelLocator
+    {
+        /// <summary>
+        /// Initializes a new instance of the ViewModelLocator class
+        /// </summary>
+        public ViewModelLocator()
+        {
+            Ioc.Default.ConfigureServices(
+                new ServiceCollection()
+                .AddSingleton<IMessenger>(new WeakReferenceMessenger())
+                .BuildServiceProvider());
+        }
+    }
+}
