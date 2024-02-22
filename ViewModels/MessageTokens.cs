9276d4be7af2581cf9f09e5f31188d1943808e77
weimin.zhang <409347105@qq.com>
2024-02-22 21:17:56 +0800

    Add messageTokens class

---
+++ ViewModels/MessageTokens.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryEditor.ViewModels
+{
+    public class MessageTokens
+    {
+        public static readonly string xxxx = nameof(xxxx);
+    }
+}
