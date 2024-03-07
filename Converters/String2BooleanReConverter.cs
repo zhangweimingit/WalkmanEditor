292ace4e676f2dff48c06457e1ac4d2d12ee4b70
weimin.zhang <409347105@qq.com>
2024-03-07 08:12:09 +0800

    添加转换器

---
+++ Converters/String2BooleanReConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WalkmanEditor.Converters
+{
+    public class String2BooleanReConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.IsNullOrEmpty((string)value) ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
