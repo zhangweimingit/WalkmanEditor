d6dfad80cc88d127b4c2789805c55f87a755e97b
zhangweimingit <409347105@qq.com>
2024-03-20 12:08:42 +0800

    Update DailyNewsTranslatePageViewModel.cs

--- ViewModels/Edit/DailyNews/DailyNewsTranslatePageViewModel.cs
+++ ViewModels/Edit/DailyNews/DailyNewsTranslatePageViewModel.cs
@@ -9,11 +9,30 @@ namespace WalkmanEditor.ViewModels.Edit.DailyNews
 {
     public class DailyNewsTranslatePageViewModel : ObservableRecipient
     {
-        public class TranslateDataModel
+        public class TranslateDataModel : ObservableRecipient
         {
-            public string English { get; set; }
+            public string English 
+            { 
+                get => m_english;
+                set
+                {
+                    m_english = value;
+                    OnPropertyChanged(nameof(English));
+                }
+            }
 
-            public string Chinese { get; set; }
+            public string Chinese 
+            { 
+                get => m_chinese;
+                set
+                {
+                    m_chinese = value;
+                    OnPropertyChanged(nameof(Chinese));
+                } 
+            }
+
+            private string m_english;
+            private string m_chinese;
         }
     }
 }
